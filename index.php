9889cc84fae7fe011c137f4c2b8535ebffd941d7
Ragnhild Kosmo Holm <ragnhildkosmo@hotmail.com>
2013-04-23 11:30:56 +0200

    Heia

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php include ('header.php'); ?>
-<?php include ('login.php'); ?>
 
-<div id="content">
-	<p> Content!</p>
+<div id="registerbutton">
+	<form><input type="button" value="Register a new account" onClick="window.location.href='http://127.0.0.1:8080/dunno/register.php'"></form>
 </div>
+
+<!-- <div id="loginbutton">
+	<form><input type="button" value="Login to existing account" onClick="window.location.href='http://127.0.0.1:8080/dunno/login.php'"></form>
+</div> -->
+<FORM> 
+<INPUT type="button" value="Login to existing account" onClick="window.open('http://127.0.0.1:8080/dunno/login.php','login','width=400,height=400')"> 
+</FORM>
+
 
 <?php include ('sidebar.php'); ?>
 <?php include ('footer.php'); ?>
