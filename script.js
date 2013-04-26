351271398869f5975e4fc14e489a734b55ab1d32
Christopher Haugen <chrihau@gmail.com>
2013-04-26 12:41:00 +0200

    Trying to add editor

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function openPublicPage(form) {
 		data: {'id': form.page.value},
 		success: function (tmp) {
 			hideEditMenu();
-			$('#Main').html('<P> Main: </P>');
+			$('#Main').html('');
 			var jsonData = jQuery.parseJSON(tmp);
 			$.each(jsonData, function (index, value) {
 				$('#Main').append('<div id=elem>'+value["content"]+'</div>');
@@ -301,7 +301,7 @@ function OpenaddPageDialog () {
 	// Show the new page dialog.	
    $("#addPage-form").dialog({
 	autoOpen: false,
-	height: 350,
+	height: 250,
 	width: 350,
 	modal: true,
 	buttons: [
@@ -317,7 +317,33 @@ function OpenaddPageDialog () {
 
 function OpenAddElementDialog () {
 	// Show the new element dialog.	
-	$('#Main').load('addNewTextElement.html');
+		$.ajax({
+		url: 'addNewTextElement.html',
+		success: function (data) {
+			$('#Main').html(data);
+			$('#text').tinymce({
+				language : 'en', 
+				// Location of TinyMCE script
+				script_url : 'tiny_mce/tiny_mce_gzip.php',
+
+				// General options
+				theme : "advanced",
+				plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,advlist,spellchecker",
+
+				// Theme options
+				theme_advanced_buttons1 : "spellchecker,iespell,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontselect,fontsizeselect,|,forecolor,backcolor",
+				theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,removeformat,help,code,|,visualchars,nonbreaking,visualaid,|,insertdate,inserttime",
+				theme_advanced_buttons3 : "tablecontrols,|,sub,sup,|,charmap,emotions,media,advhr,|,print,|,fullscreen,|,cite,abbr,acronym,del,ins",
+				theme_advanced_toolbar_location : "top",
+				theme_advanced_toolbar_align : "left",
+				theme_advanced_statusbar_location : "bottom",
+				theme_advanced_resizing : true,
+			
+				spellchecker_languages : "Norwegian=no,+English=en",
+			});
+		}
+	});
+	//$('#Main').load('addNewTextElement.html');
 };
 
 
