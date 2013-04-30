bacc50e9e37438a8faa5c5225a5a258a860102a0
Christopher Haugen <chrihau@gmail.com>
2013-04-30 23:31:04 +0200

    Trying to shorten load time

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Make a php data object
 try {
-	$db = new PDO('mysql:host=moosecloud.net;dbname=dunno', 'dunno', 'dunno');
+	$db = new PDO('mysql:host=129.242.219.39;dbname=dunno', 'dunno', 'dunno');
 } catch (PDOException $e) {
     die ('Could not connect to server : ' . $e->getMessage());
 }
